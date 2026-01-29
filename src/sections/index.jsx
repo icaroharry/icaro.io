073011fcdfe137f406bc2ec0dadfbc9f65a403df
Ícaro Harry <icaro@minka.io>
2026-01-29 15:34:06 -0300

    🔥 Remove old React/Firebase portfolio
    - Remove Create React App setup
    - Remove Firebase hosting config
    - Remove old components, sections, and hooks
    - Remove old images and assets
    - Remove old Tailwind config

--- src/sections/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header/index";
-export { default as Career } from "./Career/index";
-export { default as Projects } from "./Projects/index";
-export { default as Blog } from "./Blog";
-export { default as Footer } from "./Footer";
-export { default as Sidebar } from "./Sidebar";
